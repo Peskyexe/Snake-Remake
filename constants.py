1152edf89d7ec16c877567ef059cd36c6dbfc86d
Peskyexe <tthdarklama08@gmail.com>
2025-10-10 09:50:18 +0200

    First commit

---
+++ constants.py
@@ -0,0 +1,18 @@
+GRID_DARK = (162, 209, 73)
+GRID_LIGHT = (170, 215, 81)
+BORDER = (87, 138, 52)
+INFO_BAR = (74, 117, 44)
+
+# 17 x 15 grid
+
+# Grid square size, 50px * 50px
+# Game size, 850px * 750px
+# Game borders/padding, 40px
+# Info bar, 850px * 90px 
+
+SCREEN_WIDTH = 930
+SCREEN_HEIGHT = 920
+
+
+FRUIT_1_VALUE = 1
+FRUIT_2_VALUE = 3
---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import constants
+
+pygame.init()
+
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.set_caption("Snake")
+
