97caef2575ec2858a1751b27db5933bd1c4a6e90
Peskyexe <tthdarklama08@gmail.com>
2025-10-10 12:15:36 +0200

    Basic adaptive graphics completed

--- main.py
+++ main.py
@@ -1,8 +1,72 @@
 import pygame
-import constants
+import settings
+import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+# Gets the size of your screen for the pygame window
+screen_info = pygame.display.Info()
+screen_width, screen_height = screen_info.current_w, screen_info.current_h
+
+# Calculates the size of the game based on desired grid size
+game_width = settings.grid_size[0] * settings.GRID_SQUARE_SIZE + settings.BORDER_THICKNESS * 2
+game_height = settings.grid_size[1] * settings.GRID_SQUARE_SIZE + settings.BORDER_THICKNESS * 2 + settings.INFO_BAR_HEIGHT
+
+# Calculates where the top left corner is 
+corner_x = screen_width / 2 - game_width / 2 
+corner_y = screen_height / 2 - game_height / 2
+
+# Calculates where the grid is going to start 
+grid_start_postion_x = corner_x + settings.BORDER_THICKNESS
+grid_start_positon_y = corner_y + settings.BORDER_THICKNESS + settings.INFO_BAR_HEIGHT
+
+# Sets the pygame window size based on previously gathered values
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Snake")
 
+# Function that generates the grid based on settings
+def generate_grid():
+    x_size, y_size = settings.grid_size[0], settings.grid_size[1]
+
+    for x in range(x_size):
+        for y in range(y_size):
+            grid_position_x = grid_start_postion_x + x * settings.GRID_SQUARE_SIZE
+            grid_position_y = grid_start_positon_y + y * settings.GRID_SQUARE_SIZE
+
+            if (x + y) % 2 == 0:
+                i = 0
+            else:
+                i = 1
+
+            pygame.draw.rect(
+                screen,
+                settings.color_theme[i],
+                (grid_position_x, grid_position_y, settings.GRID_SQUARE_SIZE, settings.GRID_SQUARE_SIZE),
+            )
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+
+    # Info bar
+    pygame.draw.rect(
+        screen,
+        settings.color_theme[3],
+        (corner_x, corner_y, game_width, game_height))
+    
+    # Grid border
+    pygame.draw.rect(
+        screen, 
+        settings.color_theme[2], 
+        (corner_x, corner_y + settings.INFO_BAR_HEIGHT, game_width, game_height - settings.INFO_BAR_HEIGHT)) 
+
+    generate_grid()
+
+    pygame.display.flip()
+
+
+
+pygame.quit()
---
+++ settings.py
@@ -0,0 +1,18 @@
+GREEN_THEME = [
+    (170, 215, 81), # Grid light color
+    (152, 199, 63), # Grid dark color
+    (87, 138, 52), # Border color
+    (74, 117, 44) # Info bar color
+]
+
+GRID_SQUARE_SIZE = 50
+BORDER_THICKNESS = 30
+INFO_BAR_HEIGHT = 90
+
+FRUIT_1_VALUE = 1
+FRUIT_2_VALUE = 3
+
+SNAKE_SPEED = 6 
+
+color_theme = GREEN_THEME
+grid_size = (17, 15)
